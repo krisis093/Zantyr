bc7736e1f25754c4c681b2d366bc14d4da7b7f8e
Paweł Tomasik <pawel.tomasik@pictec.eu>
2017-08-18 00:50:50 +0200

    better deploy script

--- deployscript.sh
+++ deployscript.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# check installations
+
+REQUIREMENTS=(discord.py Flask)
+PIPFREEZE=`pip3 freeze`
+
+for module in "${REQUIREMENTS[@]}"; do
+    if grep -q "${module}" <<< "$PIPFREEZE"; then
+        echo "${module} installed"
+    else
+        pip3 install "${module}"
+    fi
+done
+
 
 git pull
-pep8 *.py --ignore=E501
-killall screen
-screen -dmS bot python3 main.py
+if pep8 *.py --ignore=E501; then
+    killall screen 2>&1 >/dev/null
+    screen -dmS bot python3 main.py
+    echo "Zantyr (re)launched!"
+else
+    "pep8 check failed, please git pull compliant version"
+fi
